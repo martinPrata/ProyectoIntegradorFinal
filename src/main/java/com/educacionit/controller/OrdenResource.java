0e7c09c2b0354ddbe16a0d428a7dbafae52f97b2
Martin <martinprata6@gmail.com>
2024-07-24 18:45:26 -0300

    Pequeña correccion

--- src/main/java/com/educacionit/controller/OrdenResource.java
+++ src/main/java/com/educacionit/controller/OrdenResource.java
@@ -7,45 +7,45 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-//
-//@RestController
-//@RequestMapping("/ordenes")
-//public class OrdenResource {
-//
-//    @Autowired
-//    private OrdenService ordenService;
-//
-//    @GetMapping
-//    public List<Orden> getAllOrdenes() {
-//        return ordenService.getAllOrdenes();
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Orden> getOrdenById(@PathVariable Long id) {
-//        Orden orden = ordenService.getOrdenById(id);
-//        if (orden == null) {
-//            return ResponseEntity.notFound().build();
-//        }
-//        return ResponseEntity.ok(orden);
-//    }
-//
-//    @PostMapping
-//    public Orden createOrden(@RequestBody Orden orden) {
-//        return ordenService.createOrden(orden);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Orden> updateOrden(@PathVariable Long id, @RequestBody Orden orden) {
-//        Orden updatedOrden = ordenService.updateOrden(id, orden);
-//        if (updatedOrden == null) {
-//            return ResponseEntity.notFound().build();
-//        }
-//        return ResponseEntity.ok(updatedOrden);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteOrden(@PathVariable Long id) {
-//        ordenService.deleteOrden(id);
-//        return ResponseEntity.noContent().build();
-//    }
-//}
+
+@RestController
+@RequestMapping("/ordenes")
+public class OrdenResource {
+
+    @Autowired
+    private OrdenService ordenService;
+
+    @GetMapping
+    public List<Orden> getAllOrdenes() {
+        return ordenService.getAllOrdenes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Orden> getOrdenById(@PathVariable Long id) {
+        Orden orden = ordenService.getOrdenById(id);
+        if (orden == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(orden);
+    }
+
+    @PostMapping
+    public Orden createOrden(@RequestBody Orden orden) {
+        return ordenService.createOrden(orden);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Orden> updateOrden(@PathVariable Long id, @RequestBody Orden orden) {
+        Orden updatedOrden = ordenService.updateOrden(id, orden);
+        if (updatedOrden == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedOrden);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrden(@PathVariable Long id) {
+        ordenService.deleteOrden(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/educacionit/dto/JwtRequest.java
+++ src/main/java/com/educacionit/dto/JwtRequest.java
@@ -9,7 +9,6 @@ public class JwtRequest implements Serializable {
     private String username;
     private String password;
 
-    // Default constructor for JSON Parsing
     public JwtRequest() {
     }
 
